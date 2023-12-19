0cfdb57beed6879086d37f3e52feb790afbaddca
iChan0415 <1christian.villafuerte@gmail.com>
2023-12-20 06:55:15 +0800

    my third commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,128 @@
+// Import React and any other necessary modules
+import React, { useEffect } from 'react';
+
+const Main = () => {
+  useEffect(() => {
+    const sign_in_btn = document.querySelector("#sign-in-btn");
+    const sign_up_btn = document.querySelector("#sign-up-btn");
+    const container = document.querySelector(".container");
+
+    const handleSignUpClick = () => {
+      container.classList.add("sign-up-mode");
+    };
+
+    const handleSignInClick = () => {
+      container.classList.remove("sign-up-mode");
+    };
+
+    sign_up_btn.addEventListener("click", handleSignUpClick);
+    sign_in_btn.addEventListener("click", handleSignInClick);
+
+    // Cleanup event listeners when the component is unmounted
+    return () => {
+      sign_up_btn.removeEventListener("click", handleSignUpClick);
+      sign_in_btn.removeEventListener("click", handleSignInClick);
+    };
+  }, []);
+
+  return (
+    <div>
+      <div className="container">
+        <div className="forms-container">
+          <div className="signin-signup">
+            <form action="#" className="sign-in-form">
+              <h2 className="title">Sign In</h2>
+              <div className="input-field">
+                <i className="fas fa-user"></i>
+                <input type="text" placeholder="Username" />
+              </div>
+              <div className="input-field">
+                <i className="fas fa-lock"></i>
+                <input type="password" placeholder="Password" />
+              </div>
+              <input type="submit" value="Login" className="btn solid" />
+              <p className="social-text">Or Continue with Social Account</p>
+              <div className="social-media">
+                <a href="#" className="social-icon">
+                  <i className="fab fa-facebook-f"></i>
+                </a>
+                <a href="#" className="social-icon">
+                  <i className="fab fa-instagram"></i>
+                </a>
+                <a href="#" className="social-icon">
+                  <i className="fab fa-twitter"></i>
+                </a>
+                <a href="#" className="social-icon">
+                  <i className="fab fa-linkedin-in"></i>
+                </a>
+              </div>
+            </form>
+            <form action="#" className="sign-up-form">
+              <h2 className="title">Sign Up</h2>
+              <div className="input-field">
+                <i className="fas fa-envelope"></i>
+                <input type="email" placeholder="Email" />
+              </div>
+              <div className="input-field">
+                <i className="fas fa-user"></i>
+                <input type="text" placeholder="Username" />
+              </div>
+              <div className="input-field">
+                <i className="fas fa-lock"></i>
+                <input type="password" placeholder="Password" />
+              </div>
+              <input type="submit" className="btn" value="Sign up" />
+              <p className="social-text">Or Continue with Social Account</p>
+              <div className="social-media">
+                <a href="#" className="social-icon">
+                  <i className="fab fa-facebook-f"></i>
+                </a>
+                <a href="#" className="social-icon">
+                  <i className="fab fa-instagram"></i>
+                </a>
+                <a href="#" className="social-icon">
+                  <i className="fab fa-twitter"></i>
+                </a>
+                <a href="#" className="social-icon">
+                  <i className="fab fa-linkedin-in"></i>
+                </a>
+              </div>
+            </form>
+          </div>
+        </div>
+
+        <div className="panels-container">
+          <div className="panel left-panel">
+            <div className="content">
+              <h3>Not a User ?</h3>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Debitis, ex ratione. Aliquid!
+              </p>
+              <button className="btn transparent" id="sign-up-btn">
+                Sign up
+              </button>
+            </div>
+            <img src="one.svg" className="image" alt="" />
+          </div>
+          <div className="panel right-panel">
+            <div className="content">
+              <h3>Already a User ?</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+                laboriosam ad deleniti.
+              </p>
+              <button className="btn transparent" id="sign-in-btn">
+                Login
+              </button>
+            </div>
+            <img src="two.svg" className="image" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
+
